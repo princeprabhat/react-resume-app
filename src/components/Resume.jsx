1b23d6be12aafe2f15d0e207473f85cf8684bfd4
princeprabhat <princeprabhat1996@gmail.com>
2025-04-12 00:24:02 +0530

    Intro section done

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -20,9 +20,32 @@ const Resume = () => {
       .save();
   };
   return (
-    <div className="bg-white font-resume p-5" ref={contentRef}>
-      <p className="text-resume-title font-bold text-5xl">hello</p>
-      <button onClick={handleDownload}>Download</button>
+    <div className="bg-gray-50 font-resume p-5" ref={contentRef}>
+      <div className="resume-introduction flex justify-between align-middle">
+        <div>
+          <p className="text-slate-800 font-bold text-4xl pb-1">
+            Prince Prabhat
+          </p>
+          <p className="text-lg font-medium text-slate-600 ml-1 ">
+            Full Stack Developer
+          </p>
+        </div>
+
+        <div className="text-sm  text-slate-700 leading-7">
+          <a href="tel:+919304692093" className="block hover:underline">
+            📞 +91-9304692093
+          </a>
+          <a
+            href="mailto:princeprabhat1996@gmail.com"
+            className="block hover:underline"
+          >
+            📧 princeprabhat1996@gmail.com
+          </a>
+          <p>🏠 Haridwar, Uttarakhand, India</p>
+        </div>
+      </div>
+
+      {/* <button onClick={handleDownload}>Download</button> */}
     </div>
   );
 };
